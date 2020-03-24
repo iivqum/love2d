debec3150a2c10a6c001170652bb5521be1efd7f
iivqum <59771955+iivqum@users.noreply.github.com>
2020-03-24 20:18:57 +0000

    Add files via upload

---
+++ proto/text.lua
@@ -0,0 +1,41 @@
+--[[
+	
+	
+]]
+
+local text = {}
+--root of double linked list
+--local head
+--cursor line
+local line
+--cursor column position
+local pos = 1
+
+function text.line(str)
+	local ln = {}
+	ln.dta = str or ""
+	if not line then
+		line = ln
+	return end
+	line.prv = ln
+	ln.nxt = line
+end
+
+function text.insert(str)
+	line.dta = table.concat({
+		line.dta:sub(1, pos)
+		str,
+		line.dta:sub(pos + 1, #line.dta)	
+	})
+	text.movecur(1, 0)
+end
+
+function text.delete()
+
+end
+
+function text.movecur(dx, dy)
+
+end
+
+return text
