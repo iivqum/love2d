194bed6c2398da3d39a394f9e825c4e885034c1d
iivqum <59771955+iivqum@users.noreply.github.com>
2020-03-24 20:29:55 +0000

    Update text.lua

--- proto/text.lua
+++ proto/text.lua
@@ -11,6 +11,10 @@ local line
 --cursor column position
 local pos = 1
 
+local function getline(n)
+	
+end
+
 function text.line(str)
 	local ln = {}
 	ln.dta = str or ""
@@ -32,14 +36,38 @@ end
 
 function text.delete()
 	line.dta = table.concat({
-		line.dta:sub(1, pos)
+		line.dta:sub(1, pos - 1)
 		line.dta:sub(pos + 1, #line.dta)	
 	})
 	text.movecur(-1, 0)
 end
 
-function text.movecur(dx, dy)
+function text.setcur(x, y)
+	local ln = getline(y)
+	if not ln then
+	return end
+	if x > #ln.dta then
+	return end
+	line = ln
+	pos = x
+	return true
+end
 
+function text.movecur(dx, dy)
+	if dx > 0 then
+		pos = math.min(#line.dta, pos + dx)
+	elseif dx < 0 then
+		pos = math.max(1, pos + dx)
+	end
+	if dy == 0 then
+	return end
+	local sig = dy / -d
+	for i = 1, math.abs(dy) do
+		local ln = getline(i * sig)
+		if not ln then
+		break end
+		line = ln
+	end
 end
 
 return text
