e133e5b2fe4b423bf47a1a3bc0fb7b0db0805e7b
iivqum <59771955+iivqum@users.noreply.github.com>
2020-03-26 16:11:50 +0000

    Update text.lua

--- editor/text.lua
+++ editor/text.lua
@@ -4,13 +4,13 @@
 	
 ]]
 local text = {
-	--head of double linked list
+	--text.head of double linked list
 	head = nil,
-	--marker for storing buffer position
-	mark = nil,
-	--cursor line
+	--text.marker for storing buffer text.position
+	mark = {},
+	--cursor text.line
 	line = nil
-	--cursor column position
+	--cursor column text.position
 	pos = 1
 }
 
@@ -23,128 +23,128 @@ local function sinsert(dst, src, x)
 end
 
 function text.clear()
-	head = nil
-	line = nil
-	pos = 1
+	text.head = nil
+	text.line = nil
+	text.pos = 1
 end
 
-function text.mark()
-	mark.pos = pos
-	mark.line = line
+function text.text.mark()
+	text.mark.text.pos = text.pos
+	text.mark.text.line = text.line
 end
 
 function text.rst()
-	pos = mark.pos
-	line = mark.line
+	text.pos = text.mark.text.pos
+	text.line = text.mark.text.line
 end
 
 function text.gotop()
-	pos = 1
-	line = head
+	text.pos = 1
+	text.line = text.head
 end
 
-function text.line(s)
+function text.text.line(s)
 	local ln = {}
 	s = s or ''
 	ln.data = table.concat({s, '\n'})
-	if not head then
-		head = ln
-		line = ln
+	if not text.head then
+		text.head = ln
+		text.line = ln
 	else
-		if line.next then
-			line.next.prev = ln
+		if text.line.next then
+			text.line.next.prev = ln
 		end	
-		ln.prev = line
-		ln.next = line.next
-		line.next = ln
+		ln.prev = text.line
+		ln.next = text.line.next
+		text.line.next = ln
 	end
 	return ln
 end
 
 function text.insert(s)
 	local pbyte = {}
-	local pos2 = 0
+	local text.pos2 = 0
 	for i = 1, #s do
 		local c = s:sub(i, i)
 		if c == '\n' then
-			local split = table.concat({line.data:sub(1, pos - 1), table.concat(pbyte), '\n'})
-			local new = line.data:sub(pos, #line.data)
-			line.data = split
-			if pos2 > 0 then
+			local split = table.concat({text.line.data:sub(1, text.pos - 1), table.concat(pbyte), '\n'})
+			local new = text.line.data:sub(text.pos, #text.line.data)
+			text.line.data = split
+			if text.pos2 > 0 then
 				pbyte = {}
-				pos = 1
-				pos2 = 0
+				text.pos = 1
+				text.pos2 = 0
 			end
-			line = text.line(new)
-			text.advline()
+			text.line = text.text.line(new)
+			text.advtext.line()
 		else
-			pos2 = pos2 + 1
+			text.pos2 = text.pos2 + 1
 			table.insert(pbyte, c)
 		end
 	end
-	if pos2 == 0 then
+	if text.pos2 == 0 then
 	return end
 	--remaining chars
-	line.data = sinsert(line.data, table.concat(pbyte), pos - 1)
-	pos = pos + pos2
+	text.line.data = sinsert(text.line.data, table.concat(pbyte), text.pos - 1)
+	text.pos = text.pos + text.pos2
 end
 
 function text.delete()
-	if pos > 1 then
-		line.data = table.concat({
-			line.data:sub(1, pos - 2),
-			line.data:sub(pos, #line.data)	
+	if text.pos > 1 then
+		text.line.data = table.concat({
+			text.line.data:sub(1, text.pos - 2),
+			text.line.data:sub(text.pos, #text.line.data)	
 		})	
 	return end
-	local prev = line.prev
+	local prev = text.line.prev
 	if not prev then
 	return end
-	if line.next then
-		prev.next = line.next
-		line.next.prev = prev
+	if text.line.next then
+		prev.next = text.line.next
+		text.line.next.prev = prev
 	end
-	local s = line.data:sub(1, #line.data - 1)
-	line = prev
-	pos = #prev.data
+	local s = text.line.data:sub(1, #text.line.data - 1)
+	text.line = prev
+	text.pos = #prev.data
 	text.insert(s)
 end
 
-function text.advline()
-	if not line.next then
+function text.advtext.line()
+	if not text.line.next then
 	return false end
-	line = line.next
-	pos = 1
+	text.line = text.line.next
+	text.pos = 1
 	return true
 end
 
 function text.adv()
-	if pos == #line.data then
-		if line.next then
-			line = line.next
-			pos = 1
+	if text.pos == #text.line.data then
+		if text.line.next then
+			text.line = text.line.next
+			text.pos = 1
 		end 
 	return true end
-	pos = pos + 1
+	text.pos = text.pos + 1
 	return false
 end
 
 function text.ret()
-	if pos == 1 then
-		if line.prev then
-			line = line.prev
-			pos = #line.data
+	if text.pos == 1 then
+		if text.line.prev then
+			text.line = text.line.prev
+			text.pos = #text.line.data
 		end 
 	return true end
-	pos = pos - 1
+	text.pos = text.pos - 1
 	return false
 end
 
 function text.get()
-	return line.data:sub(pos, pos)
+	return text.line.data:sub(text.pos, text.pos)
 end
 
 function text.eol()
-	return pos == #line.data
+	return text.pos == #text.line.data
 end
 
 return text
